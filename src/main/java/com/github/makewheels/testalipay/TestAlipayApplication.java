ceb3d57af10035170d9395142e89bbde54a06ea3
bq <email>
2021-05-18 16:30:26 +0800

    init

---
+++ src/main/java/com/github/makewheels/testalipay/TestAlipayApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.testalipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAlipayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAlipayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/testalipay/TestAlipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.testalipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAlipayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
