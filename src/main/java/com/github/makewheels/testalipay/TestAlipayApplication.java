54508bc89a672facba121cc16e4230ec13507cd5
bq <email>
2021-05-18 16:39:26 +0800

    init

--- src/main/java/com/github/makewheels/testalipay/TestAlipayApplication.java
+++ src/main/java/com/github/makewheels/testalipay/TestAlipayApplication.java
@@ -1,5 +1,10 @@
 package com.github.makewheels.testalipay;
 
+import com.alipay.easysdk.factory.Factory;
+import com.alipay.easysdk.factory.Factory.Payment;
+import com.alipay.easysdk.kernel.Config;
+import com.alipay.easysdk.kernel.util.ResponseChecker;
+import com.alipay.easysdk.payment.facetoface.models.AlipayTradePrecreateResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,8 @@ public class TestAlipayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestAlipayApplication.class, args);
+
     }
+
 
 }
