d36b23612448165a335d883ce4854f6858fa363f
makewheels <makewheels>
2021-05-19 22:14:23 +0800

    hi

--- src/main/java/com/github/makewheels/testalipay/RunTest.java
+++ src/main/java/com/github/makewheels/testalipay/RunTest.java
@@ -16,7 +16,9 @@ public class RunTest {
         try {
             // 2. 发起API调用（以创建当面付收款二维码为例）
             AlipayTradePrecreateResponse response = Payment.FaceToFace()
-                    .preCreate("Apple iPhone11 128G", "2234567890", "5799.00");
+                    .preCreate("Apple iPhone11 128G",
+                            System.currentTimeMillis() + "",
+                            "0.01");
             // 3. 处理响应或异常
             if (ResponseChecker.success(response)) {
                 System.out.println("调用成功");
@@ -35,24 +37,29 @@ public class RunTest {
         config.gatewayHost = "openapi.alipay.com";
         config.signType = "RSA2";
 
-        config.appId = "<-- 请填写您的AppId，例如：2019091767145019 -->";
+        config.appId = "2018021402198213";
 
         // 为避免私钥随源码泄露，推荐从文件中读取私钥字符串而不是写入源码中
-        config.merchantPrivateKey = "<-- 请填写您的应用私钥，例如：MIIEvQIBADANB ... ... -->";
+        config.merchantPrivateKey =
+                "D:\\Programming\\SoftTopics\\2021.05.18支付宝\\记账系统应用\\应用私钥_RSA2_PKCS8.txt";
 
-        //注：证书文件路径支持设置为文件系统中的路径或CLASS_PATH中的路径，优先从文件系统中加载，加载失败后会继续尝试从CLASS_PATH中加载
-        config.merchantCertPath = "<-- 请填写您的应用公钥证书文件路径，例如：/foo/appCertPublicKey_2019051064521003.crt -->";
-        config.alipayCertPath = "<-- 请填写您的支付宝公钥证书文件路径，例如：/foo/alipayCertPublicKey_RSA2.crt -->";
-        config.alipayRootCertPath = "<-- 请填写您的支付宝根证书文件路径，例如：/foo/alipayRootCert.crt -->";
+        //注：证书文件路径支持设置为文件系统中的路径或CLASS_PATH中的路径，优先从文件系统中加载，
+        // 加载失败后会继续尝试从CLASS_PATH中加载
+        config.merchantCertPath =
+                "D:\\Programming\\SoftTopics\\2021.05.18支付宝\\记账系统应用\\appCertPublicKey_2018021402198213.crt";
+        config.alipayCertPath =
+                "D:\\Programming\\SoftTopics\\2021.05.18支付宝\\记账系统应用\\支付宝公钥.txt";
+        config.alipayRootCertPath
+                = "D:\\Programming\\SoftTopics\\2021.05.18支付宝\\记账系统应用\\alipayRootCert.crt";
 
         //注：如果采用非证书模式，则无需赋值上面的三个证书路径，改为赋值如下的支付宝公钥字符串即可
         // config.alipayPublicKey = "<-- 请填写您的支付宝公钥，例如：MIIBIjANBg... -->";
 
         //可设置异步通知接收服务地址（可选）
-        config.notifyUrl = "<-- 请填写您的支付类接口异步通知接收服务地址，例如：https://www.test.com/callback -->";
+//        config.notifyUrl = "<-- 请填写您的支付类接口异步通知接收服务地址，例如：https://www.test.com/callback -->";
 
         //可设置AES密钥，调用AES加解密相关接口时需要（可选）
-        config.encryptKey = "<-- 请填写您的AES密钥，例如：aa4BtZ4tspm2wnXLb1ThQA== -->";
+        config.encryptKey = "SYZU541W9HNIfbl9AlK90w==";
 
         return config;
     }
--- src/main/java/com/github/makewheels/testalipay/TestAlipayApplication.java
+++ src/main/java/com/github/makewheels/testalipay/TestAlipayApplication.java
@@ -1,10 +1,5 @@
 package com.github.makewheels.testalipay;
 
-import com.alipay.easysdk.factory.Factory;
-import com.alipay.easysdk.factory.Factory.Payment;
-import com.alipay.easysdk.kernel.Config;
-import com.alipay.easysdk.kernel.util.ResponseChecker;
-import com.alipay.easysdk.payment.facetoface.models.AlipayTradePrecreateResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
